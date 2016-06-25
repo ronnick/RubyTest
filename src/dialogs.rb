9c0f19252c8243e3ecde4493c45863b23e99844d
ronnick <ronnick@163.com>
2016-06-25 12:52:38 +0800

    update

--- src/dialogs.rb
+++
@@ -1,6 +0,0 @@
-# Create the input box
-prompts = ["Title", "Message"]
-defaults = ["Dialog Title", "World peace"]
-results = inputbox prompts, defaults, "Inputbox Example"
-# Create the message box
-UI.messagebox results[1], MB_MULTILINE, results[0]
--- src/method.rb
+++
@@ -1,7 +0,0 @@
-#!usr/bin/ruby -w
-
-def say
-	puts "Hello world."
-end
-
-say
